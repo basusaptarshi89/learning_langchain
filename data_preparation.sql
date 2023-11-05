b23cace0996c2969c280a9b43544f923d8ea4464
basusaptarshi89 <basusaptarshi89@gmail.com>
2023-11-05 20:00:39 +0100

    used LLM PaLM with Langchain agent for SQL query

--- data_preparation.sql
+++ data_preparation.sql
@@ -7,10 +7,10 @@ CREATE TABLE IF NOT EXISTS public.employee (
  );
  
 INSERT INTO public.employee VALUES(1001, 'Amit', 'Verma', 1000.0, '2021-02-23');
-INSERT INTO public.employee VALUES(1001, 'Sumit', 'Agarwal', 1200.0, '2020-02-23');
-INSERT INTO public.employee VALUES(1001, 'Anuj', 'Sharma', 1500.0, '2020-02-23');
-INSERT INTO public.employee VALUES(1001, 'Deba', 'Basu', 2400.0, '2023-01-09');
-INSERT INTO public.employee VALUES(1001, 'Saptarshi', 'Bose', 2700.3, '2021-02-23');
-INSERT INTO public.employee VALUES(1001, 'Shakib', 'Khan', 3100.0, '2021-02-23');
+INSERT INTO public.employee VALUES(1002, 'Sumit', 'Agarwal', 1200.0, '2020-02-23');
+INSERT INTO public.employee VALUES(1003, 'Anuj', 'Sharma', 1500.0, '2020-02-23');
+INSERT INTO public.employee VALUES(1004, 'Deba', 'Basu', 2400.0, '2023-01-09');
+INSERT INTO public.employee VALUES(1005, 'Saptarshi', 'Bose', 2700.3, '2021-02-23');
+INSERT INTO public.employee VALUES(1006, 'Shakib', 'Khan', 3100.0, '2021-02-23');
 
 select * from public.employee;
